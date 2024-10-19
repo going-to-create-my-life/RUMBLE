9a4a3368e92800cdabc766b2893a8335174c54c5
Sourav <souravsingla335@gmail.com>
2024-10-20 02:49:39 +0530

    Auth completed

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const cors = require('cors'); // Import the cors package
 const PORT = process.env.PORT || 5000;
 require('./google')
 const app = express();
@@ -17,7 +18,9 @@ app.use(session({
 // Initialize Passport.js
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(cors({
+    origin: 'http://localhost:3000' // Allow only requests from this origin
+}));
 // Route to start the authentication process
 app.get('/auth/google',
     passport.authenticate('google', { scope: ['profile', 'email'] })
@@ -48,10 +51,24 @@ app.get('/add', (req, res) => {
     const token2 = jwt.sign(decoded, SECRET,);
 
     // Send the token back to the client
-    res.redirect(`http://localhost:3000/home?token=${token2}`); 
+    res.redirect(`http://localhost:3000/dashboard?token=${token2}`);
 
 });
 
+app.get('/authen', (req, res) => {
+    const token = req.query.token;
+    console.log(token);
+    if (!token) {
+        return res.status(400).json({ message: 'Token is required' });
+    }
+    try {
+        const decoded = jwt.verify(token, SECRET);
+        if(decoded['add'] == "NULL") res.send("WALLET");
+        else res.send("SAFE");
+    } catch (err) {
+        res.send("LOGIN");
+    }
+});
 
 // Start the server
 app.listen(PORT, () => {
